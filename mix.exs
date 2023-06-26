38789f300dbd4f2ce305f6575cc8dc54a802f9d6
Mark Markaryan <markmark206@gmail.com>
2023-06-25 17:26:24 -0700

    updating readme and adding package information to mix.exs

--- mix.exs
+++ mix.exs
@@ -6,12 +6,25 @@ defmodule Nopass.MixProject do
       app: :nopass,
       version: "0.1.0",
       elixir: "~> 1.14",
+      name: "Nopass",
+      description:
+        "Nopass simplifies managing magic codes (aka 'one-time passwords') and login tokens for passwordless experiences.",
+      package: [
+        name: "nopass",
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/markmark206/nopass"}
+      ],
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/markmark206/nopass",
       test_coverage: [
         summary: [
-          threshold: 90
+          threshold: 100
         ],
         ignore_modules: [Nopass.Repo]
+      ],
+      docs: [
+        main: "Nopass",
+        extras: ["README.md", "LICENSE"]
       ],
       deps: deps()
     ]
